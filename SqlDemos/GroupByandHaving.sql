a3e42226ac742c7d21e68db961f732c4068bc017
chaud <chaud@BHUSHAN>
2022-09-21 10:50:43 +0530

    GroupByHaving

---
+++ SqlDemos/GroupByandHaving.sql
@@ -0,0 +1,164 @@
+create database CustomerDB1
+go
+use CustomerDB1
+go
+
+create table customer1(
+CustomerID int primary key identity(11,1),
+CustomerName varchar(50) not null,
+ContactName	varchar(50) not null,
+Address	varchar(50) not null,
+City varchar(50) not null,
+PostalCode	varchar(50) not null,
+Country varchar(50) not null
+);
+go
+insert into customer1
+values('Alfreds Futterkiste','Maria Anders','Obere Str. 57','Berlin','12209','Germany')
+insert into customer1
+values('Ana Trujillo Emparedados y helados','Ana Trujillo','Avda. de la Constitución 2222','México D.F.','05021','Mexico')
+insert into customer1
+values('Antonio Moreno Taquería','Antonio Moreno','Mataderos 2312','México D.F.','05023','Mexico')
+insert into customer1
+values('Around the Horn','Thomas Hardy','120 Hanover Sq.','London','WA1 1DP','UK')
+insert into customer1
+values('Berglunds snabbköp','Christina Berglund','Berguvsvägen 8','Luleå','S-958 22','Sweden')
+go
+--select all record
+select * from customer1;
+go
+--selects the "CustomerName" and "City" columns from the "Customers" table
+SELECT CustomerName, City FROM customer1;
+
+--DISTINCT -selects all (including the duplicates) values from the "Country" column in the "Customers" table
+SELECT Country FROM customer1;
+
+--selects only the DISTINCT values from the "Country" column in the "Customers" table
+SELECT DISTINCT Country FROM customer1;
+
+--lists the number of different (distinct) customer countries
+SELECT COUNT(DISTINCT Country) FROM customer1;
+go
+
+--WHERE -
+
+--selects all the customers from the country "Mexico", in the "Customers" table
+SELECT * FROM customer1
+WHERE Country='Mexico';
+
+--OerderBy-
+
+--selects all customers from the "Customers" table, sorted by the "Country" column
+SELECT * FROM customer1
+ORDER BY Country;
+
+--selects all customers from the "Customers" table, sorted DESCENDING by the "Country" column
+SELECT * FROM customer1
+ORDER BY Country DESC;
+
+-- selects all customers from the "Customers" table, sorted by the "Country" and the "CustomerName" column. This means that it orders by Country, but if some rows have the same Country, it orders them by CustomerName
+SELECT * FROM customer1
+ORDER BY Country, CustomerName;
+
+-- selects all customers from the "Customers" table, sorted ascending by the "Country" and descending by the "CustomerName" column
+SELECT * FROM customer1
+ORDER BY Country ASC, CustomerName DESC;
+
+
+--UPDATE-updates the first customer (CustomerID = 1) with a new contact person and a new city
+UPDATE customer1
+SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
+WHERE CustomerID = 1;
+
+select * from customer1
+
+--update the ContactName to "Juan" for all records where country is "Mexico":
+UPDATE customer1
+SET ContactName='Juan'
+WHERE Country='Mexico';
+
+--ALL records will be updated
+UPDATE customer1
+SET ContactName='Juan';
+
+--DELETE-deletes the customer "Alfreds Futterkiste" from the "Customers" table
+DELETE FROM customer1 WHERE CustomerName='Alfreds Futterkiste';
+
+DELETE FROM customer1;
+
+select * from customer1
+
+--WildCards-selects all customers with a City starting with "L"
+SELECT * FROM customer1
+WHERE City LIKE 'L%';
+
+--selects all customers with a City ending with "F."
+SELECT * FROM customer1
+WHERE City LIKE '%F.';
+
+--selects all customers with a City starting with any character, followed by "ondon"
+SELECT * FROM customer1
+WHERE City LIKE '_ondon';
+
+--IN -selects all customers that are located in "Germany", "France" or "UK":
+SELECT * FROM customer1
+WHERE Country IN ('Germany', 'France', 'UK');
+
+--selects all customers that are NOT located in "Germany", "France" or "UK":
+SELECT * FROM customer1
+WHERE Country NOT IN ('Germany', 'France', 'UK');
+
+--BETWEEN-
+create table Product1(
+ProductID int primary key identity(1,1),
+ProductName varchar(30) not null,
+SupplierID	int not null,
+CategoryID	int not null,
+Unit	varchar(30)not null,
+Price int not null
+);
+
+insert into Product1
+values('Chais',1,1,'boxes x 20 bags',18)
+insert into Product1
+values('Chang',1,1,'24 - 12 oz bottles',19)
+insert into Product1
+values('Aniseed Syrup',1,2,'	12 - 550 ml bottles',10)
+insert into Product1
+values('Chef Antons Cajun Seasoning',1,2,'	48 - 6 oz jars',22)
+insert into Product1
+values('Chef Antons Gumbo Mix',1,2,'	36 boxes',21.35)
+
+select * from Product1
+
+--selects all products with a price between 10 and 20:
+SELECT * FROM Product1
+WHERE Price BETWEEN 10 AND 20;
+
+
+SELECT * FROM Product1
+WHERE Price NOT BETWEEN 10 AND 20;
+
+--selects all products with a ProductName between Carnarvon Tigers and Mozzarella di Giovanni:
+SELECT * FROM Product1
+WHERE ProductName BETWEEN 'Carnarvon Tigers' AND 'Mozzarella di Giovanni'
+ORDER BY ProductName;
+
+--GroupBY And Having
+-- lists the number of customers in each country:
+SELECT COUNT(CustomerID), Country
+FROM customer1
+GROUP BY Country;
+
+-- lists the number of customers in each country, sorted high to low:
+SELECT COUNT(CustomerID), Country
+FROM customer1
+GROUP BY Country
+ORDER BY COUNT(CustomerID) DESC;
+
+--Having
+SELECT COUNT(CustomerID), Country
+FROM customer1
+GROUP BY Country
+HAVING COUNT(CustomerID) > 5
+ORDER BY COUNT(CustomerID) DESC;
