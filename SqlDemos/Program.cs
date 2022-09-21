3e5e92606df2f5b8556d1363eaed9b0c43ef5710
chaud <chaud@BHUSHAN>
2022-09-21 10:34:22 +0530

    Add project files.

---
+++ SqlDemos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SqlDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
